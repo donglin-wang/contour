3bccc6ed968364af4650e7ad723703aa96db1139
Donglin-Wang <wdl448@yahoo.com>
2025-05-14 19:53:22 -0400

    Accordion first draft

--- js/icon.js
+++ js/icon.js
@@ -92,9 +92,43 @@ class IconChevronRight extends IconBase {
     }
 }
 
+class IconChevronDown extends IconBase {
+    getSVG(strokeWidth, fill) {
+        return /* html */`
+            <svg xmlns="http://www.w3.org/2000/svg" stroke-width="${strokeWidth}" fill="${fill}" viewBox="0 0 16 16" class="icon">
+                <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/>
+            </svg>
+        `
+    }
+}
+
+class IconChevronUp extends IconBase {
+    getSVG(strokeWidth, fill) {
+        return /* html */`
+            <svg xmlns="http://www.w3.org/2000/svg" stroke-width="${strokeWidth}" fill="${fill}" viewBox="0 0 16 16" class="icon">
+                <path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708z"/>
+            </svg>
+        `
+    }
+}
+
+class IconBackpack extends IconBase {
+    getSVG(strokeWidth, fill) {
+        return /* html */`
+            <svg xmlns="http://www.w3.org/2000/svg" stroke-width="${strokeWidth}" fill="${fill}" viewBox="0 0 16 16" class="icon">
+                <path d="M4.04 7.43a4 4 0 0 1 7.92 0 .5.5 0 1 1-.99.14 3 3 0 0 0-5.94 0 .5.5 0 1 1-.99-.14M4 9.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5zm1 .5v3h6v-3h-1v.5a.5.5 0 0 1-1 0V10z"/>
+                <path d="M6 2.341V2a2 2 0 1 1 4 0v.341c2.33.824 4 3.047 4 5.659v5.5a2.5 2.5 0 0 1-2.5 2.5h-7A2.5 2.5 0 0 1 2 13.5V8a6 6 0 0 1 4-5.659M7 2v.083a6 6 0 0 1 2 0V2a1 1 0 0 0-2 0m1 1a5 5 0 0 0-5 5v5.5A1.5 1.5 0 0 0 4.5 15h7a1.5 1.5 0 0 0 1.5-1.5V8a5 5 0 0 0-5-5"/>
+            </svg>
+        `
+    }
+}
+
 customElements.define('icon-exclamation-square-fill', IconExclamationSquareFill);
 customElements.define('icon-alarm', IconAlarm);
 customElements.define('icon-x', IconX);
 customElements.define('icon-plus', IconPlus);
 customElements.define('icon-check', IconCheck);
-customElements.define('icon-chevron-right', IconChevronRight)
+customElements.define('icon-chevron-right', IconChevronRight);
+customElements.define('icon-chevron-down', IconChevronDown);
+customElements.define('icon-chevron-up', IconChevronUp);
+customElements.define('icon-backpack', IconBackpack);
