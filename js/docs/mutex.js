60dc773f034d5efd99870cc45c4759aafc1bb0b8
Donglin-Wang <wdl448@yahoo.com>
2025-12-19 20:24:48 -0500

    Indicated variance

--- js/docs/mutex.js
+++ js/docs/mutex.js
@@ -279,6 +279,69 @@ const tabJoint = div(
     )
 );
 
+const tabIndecatedStyle = registerStyle(/*css*/`
+@layer {
+    .mutex[data-variant="tab-indicated"] {
+        --mutex-item-color: var(--color-3);
+        --mutex-item-hover-color: var(--color-1);
+        --mutex-item-active-color: var(--color-1);
+        --mutex-padding-block: var(--space-1);
+        --mutex-padding-inline: var(--space-1);
+    
+        background: var(--background-color-2);
+        border-radius: 3px;
+    }
+
+    .mutex__indicator[data-variant="tab-indicated"] {
+        --mutex-indicator-background: var(--background-color-1);
+        --mutex-indicator-border-radius: 3px;
+        --mutex-indicator-box-shadow: 0 1px 1px 1px rgba(0, 0, 0, 0.05);
+        height: calc(100% - 2 * var( --mutex-padding-block));
+        transition: left 0.2s ease, width 0.2s ease;
+        position: absolute;
+        left: var(--mutex-padding-inline);
+    }
+
+    .mutex__item[data-variant="tab-indicated"][aria-selected] {
+        color: var(--color-1);
+    }
+}`);
+
+const tabIndicated = div(
+    {
+        class: "mutex",
+        "data-variant": "tab-indicated",
+    },
+    div(
+        {
+            class: "mutex__indicator",
+            "data-variant": "tab-indicated",
+        }
+    ),
+    div(
+        {
+            class: "mutex__item",
+            "data-variant": "tab-indicated",
+            "aria-selected": null,
+        },
+        "Tab"
+    ),
+    div(
+        {
+            class: "mutex__item",
+            "data-variant": "tab-indicated",
+        },
+        "Long tab"
+    ),
+    div(
+        {
+            class: "mutex__item",
+            "data-variant": "tab-indicated",
+        },
+        "Even longer tab"
+    )
+);
+
 const fileTabStyle = registerStyle(/*css*/ `
 @layer variant {
     .mutex__item[data-variant="file-tab"] {
@@ -442,21 +505,44 @@ const article = Article(
     Section(tabOutlined),
     Section(tabJoint),
     Section(fileTab),
-    Section(dock)
+    Section(dock),
+    Section(tabIndicated)
 );
 
 van.add(document.body, article);
 
+for (const indicator of document.getElementsByClassName("mutex__indicator")) {
+    const parent = indicator.parentElement;
+    const firstItem = parent.getElementsByClassName("mutex__item").item(0);
+    indicator.style.width = `${firstItem.getBoundingClientRect().width}px`;
+}
+
 for (const mutexItem of document.getElementsByClassName("mutex__item")) {
     mutexItem.addEventListener("click", () => {
-        const siblings =
-            mutexItem.parentElement.getElementsByClassName("mutex__item");
+        const parent = mutexItem.parentElement;
+        const siblings = parent.children;
+
+        let indicator = null;
         for (const sibling of siblings) {
             if (sibling.getAttribute("aria-selected") !== null) {
                 sibling.toggleAttribute("aria-selected");
-                break;
+            }
+
+            if (sibling.classList.contains("mutex__indicator")) {
+                indicator = sibling;
             }
         }
+    
         mutexItem.setAttribute("aria-selected", "");
+
+        if (indicator !== null) {
+            const mutexRect = parent.getBoundingClientRect();
+            const itemRect = mutexItem.getBoundingClientRect();
+            const left = itemRect.left - mutexRect.left;
+            const width = itemRect.width;
+
+            indicator.style.left = `${left}px`;
+            indicator.style.width = `${width}px`;
+        }
     });
 }
