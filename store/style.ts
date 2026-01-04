775cb83895a5f9d3804028d8f202f73e6bf60ea4
Donglin-Wang <wdl448@yahoo.com>
2026-01-04 12:13:01 -0500

    Refactored style registration

---
+++ store/style.ts
@@ -0,0 +1,21 @@
+let current: string;
+
+export const resetStyle = () => {
+    document.adoptedStyleSheets = [];
+    current = "";
+};
+
+export const setCurrentStyle = async (key: string, ...styleStrings: string[]) => {
+    if (current === key) {
+        return;
+    }
+
+    resetStyle();
+
+    const sheet = await new CSSStyleSheet().replace(styleStrings.join());
+    document.adoptedStyleSheets.push(sheet);
+
+    current = key;
+};
+
+export const initializeStyleRegistry = () => (current = "");
