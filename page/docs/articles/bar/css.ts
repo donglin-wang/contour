ba4fa312cd4bd21ef6ee53129426163895e3ca9a
Donglin-Wang <wdl448@yahoo.com>
2026-02-07 12:49:07 -0500

    Eliminated logo text variant

--- page/docs/articles/bar/css.ts
+++ page/docs/articles/bar/css.ts
@@ -1,6 +1,5 @@
 import sections from "/style/variant/bar/modifiers.css?inline";
 import iconTrigger from "/style/variant/trigger/icon.css?inline";
 import iconModifiers from "/style/variant/trigger/modifiers.css?inline";
-import logoText from "/style/variant/text/logo.css?inline";
 
-export default sections + iconTrigger + iconModifiers + logoText;
+export default sections + iconTrigger + iconModifiers;
--- page/home/css.ts
+++ page/home/css.ts
@@ -1,4 +1,3 @@
-import textLogo from "/style/variant/text/logo.css?inline";
 import textProse from "/style/variant/text/prose.css?inline";
 import homeStyle from "/page/home/style.css?inline";
 import triggerIcon from "/style/variant/trigger/icon.css?inline";
@@ -6,8 +5,7 @@ import triggerModifier from "/style/variant/trigger/modifiers.css?inline";
 import barSections from "/style/variant/bar/modifiers.css?inline";
 import barTopNav from "/style/variant/bar/topNav.css?inline";
 
-export default textLogo +
-    textProse +
+export default textProse +
     homeStyle +
     triggerIcon +
     triggerModifier +
