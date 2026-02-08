389e851ddd1acf3d843603033fba757d6761d350
Donglin-Wang <wdl448@yahoo.com>
2026-02-08 11:58:41 -0500

    Formatting & removed unused styles

--- page/home/css.ts
+++ page/home/css.ts
@@ -1,12 +1,10 @@
-import textProse from "/style/variant/text/prose.css?inline";
 import homeStyle from "/page/home/style.css?inline";
 import triggerIcon from "/style/variant/trigger/icon.css?inline";
 import triggerModifier from "/style/variant/trigger/modifiers.css?inline";
 import barSections from "/style/variant/bar/modifiers.css?inline";
 import barTopNav from "/style/variant/bar/topNav.css?inline";
 
-export default textProse +
-    homeStyle +
+export default homeStyle +
     triggerIcon +
     triggerModifier +
     barSections +
--- page/home/routes.ts
+++ page/home/routes.ts
@@ -5,9 +5,10 @@ export default [
     {
         pattern: "",
         callback: async (root: HTMLElement) => {
-            const homeElement = await import("/page/home/root")
-            await setCurrentStyle("home", homeStyle)
+            const homeElement = await import("/page/home/root");
+            await setCurrentStyle("home", homeStyle);
             root.replaceChildren(homeElement.default);
+            return root;
         },
         children: [],
     },
